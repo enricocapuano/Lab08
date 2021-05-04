68ef510421f44f928b9d808d6c7b92554cbe3d27
enric <enric@PC-Enrico>
2021-05-04 12:19:31 +0200

    versione 1

--- src/main/java/it/polito/tdp/extflightdelays/db/ExtFlightDelaysDAO.java
+++ src/main/java/it/polito/tdp/extflightdelays/db/ExtFlightDelaysDAO.java
@@ -5,8 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.extflightdelays.model.Airline;
 import it.polito.tdp.extflightdelays.model.Airport;
@@ -37,9 +39,8 @@ public class ExtFlightDelaysDAO {
 		}
 	}
 
-	public List<Airport> loadAllAirports() {
+	public void loadAllAirports(Map<Integer, Airport> map) {
 		String sql = "SELECT * FROM airports";
-		List<Airport> result = new ArrayList<Airport>();
 
 		try {
 			Connection conn = ConnectDB.getConnection();
@@ -47,14 +48,16 @@ public class ExtFlightDelaysDAO {
 			ResultSet rs = st.executeQuery();
 
 			while (rs.next()) {
-				Airport airport = new Airport(rs.getInt("ID"), rs.getString("IATA_CODE"), rs.getString("AIRPORT"),
-						rs.getString("CITY"), rs.getString("STATE"), rs.getString("COUNTRY"), rs.getDouble("LATITUDE"),
-						rs.getDouble("LONGITUDE"), rs.getDouble("TIMEZONE_OFFSET"));
-				result.add(airport);
+				if(!map.containsKey(rs.getInt("ID"))) {
+					Airport airport = new Airport(rs.getInt("ID"), rs.getString("IATA_CODE"), rs.getString("AIRPORT"),
+							rs.getString("CITY"), rs.getString("STATE"), rs.getString("COUNTRY"), rs.getDouble("LATITUDE"),
+							rs.getDouble("LONGITUDE"), rs.getDouble("TIMEZONE_OFFSET"));
+					map.put(airport.getId(), airport);
+				}
+				
 			}
 
 			conn.close();
-			return result;
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -80,6 +83,37 @@ public class ExtFlightDelaysDAO {
 						rs.getDouble("ELAPSED_TIME"), rs.getInt("DISTANCE"),
 						rs.getTimestamp("ARRIVAL_DATE").toLocalDateTime(), rs.getDouble("ARRIVAL_DELAY"));
 				result.add(flight);
+			}
+
+			conn.close();
+			return result;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Errore connessione al database");
+			throw new RuntimeException("Error Connection Database");
+		}
+	}
+	
+	public Map<Airport, Integer> getVoli(int id1){
+		String sql = "SELECT *, AVG(DISTANCE) as media "
+				+ "FROM flights, airports "
+				+ "WHERE (ORIGIN_AIRPORT_ID = ? OR DESTINATION_AIRPORT_ID = ?) "
+				+ "GROUP BY ORIGIN_AIRPORT_ID";
+		Map<Airport, Integer> result = new HashMap<>();
+		
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, id1);
+			st.setInt(2, id1);
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+				Airport airport = new Airport(rs.getInt("ID"), rs.getString("IATA_CODE"), rs.getString("AIRPORT"),
+						rs.getString("CITY"), rs.getString("STATE"), rs.getString("COUNTRY"), rs.getDouble("LATITUDE"),
+						rs.getDouble("LONGITUDE"), rs.getDouble("TIMEZONE_OFFSET"));
+				result.put(airport, (int)rs.getDouble("media"));
 			}
 
 			conn.close();
--- src/main/java/it/polito/tdp/extflightdelays/model/Model.java
+++ src/main/java/it/polito/tdp/extflightdelays/model/Model.java
@@ -1,5 +1,61 @@
 package it.polito.tdp.extflightdelays.model;
 
-public class Model {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.extflightdelays.db.ExtFlightDelaysDAO;
+
+
+public class Model {
+	
+	Graph<Airport, DefaultWeightedEdge> grafo;
+	Map<Integer, Airport> idMap;
+	ExtFlightDelaysDAO dao;
+	List<Flight> voli;
+	
+	public Model() {
+		idMap = new HashMap<Integer, Airport>();
+		dao = new ExtFlightDelaysDAO();
+		voli = dao.loadAllFlights();
+	}
+	
+	
+	public void creaGrafo(int distanza) {
+		this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);	
+		dao.loadAllAirports(idMap);
+		Graphs.addAllVertices(this.grafo, idMap.values());
+		
+		for(Airport a1 : this.grafo.vertexSet()) {
+			
+			Airport a2 = null;	
+			Map<Airport, Integer> mappa = dao.getVoli(a1.getId());
+			int media = 0;
+			for(Airport a : mappa.keySet()) {
+				media += mappa.get(a);
+				a2 = a;
+			}
+			
+			media = media/2;
+			if(media >= distanza) {
+				Graphs.addEdge(this.grafo, idMap.get(a1.getId()), idMap.get(a2.getId()), media);
+			}
+
+				
+			
+		}
+		System.out.println("GRAFO CREATO!");
+		System.out.println("# vertici: "+this.grafo.vertexSet().size());
+		System.out.println("# archi: "+this.grafo.edgeSet().size());
+	}
+	
+	
+	
+	
 }
